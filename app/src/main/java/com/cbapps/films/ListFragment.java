91bf42d97b315e471e46435ae2bf1c8bbb9586b3
Coen <boelhouwers.coen@gmail.com>
2017-04-25 16:27:54 +0200

    Start implementing multi-pane.

---
+++ app/src/main/java/com/cbapps/films/ListFragment.java
@@ -0,0 +1,21 @@
+package com.cbapps.films;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author CoenB95
+ */
+
+public class ListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.movie_list, container, false);
+    }
+}
